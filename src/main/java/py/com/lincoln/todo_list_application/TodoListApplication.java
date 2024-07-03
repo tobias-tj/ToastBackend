51fb472fdace2b834638dd60a1865f7c73b15c16
osramirezlincoln <oramirez@lincoln.com.py>
2024-07-03 16:56:08 -0400

    Initial commit

---
+++ src/main/java/py/com/lincoln/todo_list_application/TodoListApplication.java
@@ -0,0 +1,13 @@
+package py.com.lincoln.todo_list_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApplication.class, args);
+	}
+
+}
